d547fec1fe9ad0669fccd49de1fa5a1de2053f0e
markdaniel1982 <>
2023-05-18 08:59:20 +0000

    change content theme and adjust styling

--- md82/settings.py
+++ md82/settings.py
@@ -162,6 +162,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+FAVICON_URL = BASE_DIR
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
