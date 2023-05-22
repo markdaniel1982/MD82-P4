1fad6da875de92acff868370d2e238004d505d0d
markdaniel1982 <>
2023-05-22 15:36:41 +0000

    add number of comments shown under each blog post on main page

--- blog/models.py
+++ blog/models.py
@@ -30,6 +30,9 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+    def number_of_comments(self):
+        return self.comments.count()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
