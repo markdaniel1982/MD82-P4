2ef680c3d88f590c61869762ac04e2df0915567d
markdaniel1982 <>
2023-05-25 07:37:38 +0000

    remove default author from Post in  models.py

--- blog/models.py
+++ blog/models.py
@@ -21,8 +21,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts", default='Admin'
-    )
+        User, on_delete=models.CASCADE, related_name="blog_posts")
     featured_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
